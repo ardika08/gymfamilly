6e275e7ee4354b559658843dd81908c70051a987
Ubuntu <ubuntu@localhost.localdomain>
2026-08-11 17:59:20 +0700

    fix: sync Laravel SPA assets with frontend build

--- backend/resources/views/spa.blade.php
+++ backend/resources/views/spa.blade.php
@@ -11,7 +11,8 @@
       content="Gym Familly membantu member dan admin mengelola membership gym dengan cepat."
     />
     <title>Gym Familly</title>
-    <script type="module" crossorigin src="/assets/index-BsLMI3gI.js"></script>
+    <script type="module" crossorigin src="/assets/index-D6EcEPAK.js"></script>
+    <link rel="stylesheet" crossorigin href="/assets/index-Dxjt6y6l.css">
   </head>
   <body>
     <div id="root"></div>
